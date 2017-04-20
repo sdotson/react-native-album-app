567d56d3922ec96ad64d3bf8b0d21184151313a6
sdotson <stu.dotson@gmail.com>
2017-04-20 15:58:36 -0400

    react native app done

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album } = props ) => {
+  const { thumbnail_image, title, artist, image, url } = album;
+  return (
+    <Card>
+      <CardSection>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            source={{uri: thumbnail_image }}
+            style={styles.thumbnailStyle}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{uri: image }}
+          style={styles.imageStyle}
+        />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+}
+
+export default AlbumDetail;
---
+++ src/components/AlbumList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Text, ScrollView } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  constructor(props){
+    super(props);
+    this.state = { albums: [] };
+  }
+
+  componentDidMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => this.setState({ albums: data }));
+  }
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+  render() {
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
+
+export default AlbumList;
